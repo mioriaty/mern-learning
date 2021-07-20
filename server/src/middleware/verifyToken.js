1b9acffbd20b1dd2c6d653be46f2af6abad71ebc
mioriaty <mioriaty@gmail.com>
2021-07-20 20:56:03 +0700

    add controller post

--- server/src/middleware/verifyToken.js
+++ server/src/middleware/verifyToken.js
@@ -1,4 +1,4 @@
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 
 const verifyToken = (req, res, next) => {
   const authHeader = req.header('Authorization');
@@ -7,7 +7,7 @@ const verifyToken = (req, res, next) => {
   if (!token) {
     return res.status(401).json({
       success: false,
-      messsage: "Access token not found!"
+      messsage: "Access token not found!",
     });
   }
   try {
@@ -15,14 +15,13 @@ const verifyToken = (req, res, next) => {
     const decoded = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
     req.userId = decoded.userId;
     next();
-
   } catch (error) {
     console.log(error);
     return res.status(403).json({
       success: false,
-      message: 'Invalid token'
-    })
+      message: "Invalid token",
+    });
   }
-}
+};
 
 module.exports = verifyToken;
